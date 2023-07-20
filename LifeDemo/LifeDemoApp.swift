7324111f7c8dfcc2554cb0a900e0ed28993d40be
lvxuzijian <lvxuzijian@outlook.com>
2023-07-20 21:35:12 +0800

    Initial Commit

---
+++ LifeDemo/LifeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeDemoApp.swift
+//  LifeDemo
+//
+//  Created by ZijianXu on 2023/7/20.
+//
+
+import SwiftUI
+
+@main
+struct LifeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
